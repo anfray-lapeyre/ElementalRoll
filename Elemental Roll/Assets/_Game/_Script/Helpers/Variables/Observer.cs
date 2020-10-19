9ec078acd0b407546937e4091bcacaa8c93b301e
anfray-lapeyre <contact@anfraylapeyre.com>
2020-10-19 10:47:46 +0200

    Observer first added

---
+++ Elemental Roll/Assets/_Game/_Script/Helpers/Variables/Observer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Observer : MonoBehaviour
+{
+
+   public void OnNotify(GameObject entity, Object notifiedEvent) { }
+   public int ID=-1;
+}
---
+++ Elemental Roll/Assets/_Game/_Script/Helpers/Variables/Subject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Subject : MonoBehaviour
+{
+
+    private  Observer[] observers;
+    private int numObservers=0;
+
+    public void addObserver(Observer _observer)
+    {
+        observers[numObservers] = _observer;
+        _observer.ID = numObservers;
+        numObservers++;
+    }
+
+    public void removeObserver(Observer _observer)
+    {
+        //We get the observers ID
+        int newID = _observer.ID;
+        numObservers--;
+        //Then replace it with the last observer
+        observers[newID] = observers[numObservers];
+        //And change its own ID
+        //And change its own ID
+        observers[newID].ID = newID;
+    }
+}
